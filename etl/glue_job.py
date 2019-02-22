68b5ff99d5505778507c1f7fdea4ebe5d7fa82eb
Alex <agrvs@users.noreply.github.com>
2019-02-22 12:54:36 -0700

    Create glue_job.py

---
+++ etl/glue_job.py
@@ -0,0 +1,8 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+
