067b54a05374b86962ddac160f2ddaaf5c60708c
Alex <alex@1strategy.com>
2019-02-26 13:35:41 -0700

    add notebook for local dev

--- notebooks/segmentation.py
+++ notebooks/segmentation.py
@@ -1,4 +1,13 @@
 #%%
-import matplotlib.pyplot as plt
-
 import sagemaker
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+#%%
+
+ozone = pd.read_csv(
+    "/Users/alexgraves/Desktop/airquality/ozone_per_day.csv"
+)
