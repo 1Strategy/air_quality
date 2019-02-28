128156ba1d84b629e6ce3062bcfeacea65561b5f
Alex <alex@1strategy.com>
2019-02-28 09:38:55 -0700

    set glue job to pull directly from aurora

--- etl/utah_subset.py
+++ etl/utah_subset.py
@@ -16,7 +16,8 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Tables with important attributes
-ozone = glueContext.create_dynamic_frame.from_catalog(database = "1strategy-insights-dm", table_name = "insights_dm_stage_cached_applications", transformation_ctx = "applications")
+# ozone = glueContext.create_dynamic_frame.from_catalog(database = "1strategy-insights-dm", table_name = "insights_dm_stage_cached_applications", transformation_ctx = "applications")
+ozone = connection_options = {"url": "jdbc:mysql://air-quality.crwizazpv2rg.us-west-2.rds.amazonaws.com:3306/airquality", "user": "etl", "password": "thisismysupersecretpassword", "dbtable": "ozone"}
 
 # Create Data Frames and Views to query
 ozone_df = ozone.toDF()
